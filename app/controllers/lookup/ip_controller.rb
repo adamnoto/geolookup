a29b8a3abd684eec8f4eeb06bad6015a416d2ff5
Adam Pahlevi <adam.pahlevi@gmail.com>
2018-10-03 11:43:32 +0900

    note finding out ip text

--- app/controllers/lookup/ip_controller.rb
+++ app/controllers/lookup/ip_controller.rb
@@ -4,6 +4,7 @@ class Lookup::IpController < ApplicationController
 
   def show
     ip = params[:id] || "127.0.0.1"
+    Rails.logger.info "Finding out: #{ip}"
     render json: LOOKUP.city(ip).to_h, content_type: :json
   end
 end
